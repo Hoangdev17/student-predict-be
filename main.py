88e6c24e85d0800e144970f5f788f5066315f1a6
Hoangdev17 <hoangluudev17@gmail.com>
2025-12-12 11:23:00 +0700

    fix

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://student-predict-smoky.vercel.app/"
+    "https://student-predict-smoky.vercel.app"
 ]
 
 app.add_middleware(
